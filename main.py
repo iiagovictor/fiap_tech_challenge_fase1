3de7cabe83c26c10e8125780207782c4fc834c8a
JDSdamasceno <jadersondamascenodelima@gmail.com>
2025-06-21 10:04:30 -0300

    Criação do Main

---
+++ main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+
+app = FastAPI(
+    title="Our FastAPI API",
+    version="1.0.0",
+    description="API de Consultas de Livros com FASTAPI"
+)
+
+@app.get("/")
+async def home():
+    return "Página Inicial!"
