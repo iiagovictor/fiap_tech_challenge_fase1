6b7a12b17950312a719325c01176dd5bd56383bc
Vitor Santiago <vitorsantiago14101997@yahoo.com>
2025-06-22 10:38:17 -0300

    Subindo alterações para leitura de arquivo csv.

--- app/api/v1/endpoint_categoria.py
+++ app/api/v1/endpoint_categoria.py
@@ -1,6 +1,11 @@
 from fastapi import APIRouter
+from fiap_tech_challenge_fase1.app.utils.helpers import get_csv_data
 
 router = APIRouter()
+
+dados_csv = get_csv_data(rf'C:\Users\Victor\Desktop\CONEXAO_REPO_fiap_tech_challenge_fase1\fiap_tech_challenge_fase1\data\books.csv')
+
+print(dados_csv)
 
 @router.get("/")
 async def get_categories():
--- app/main.py
+++ app/main.py
@@ -1,11 +1,17 @@
 from fastapi import FastAPI
 from app.api.v1 import endpoint_categoria
+from fiap_tech_challenge_fase1.app.utils.helpers import get_csv_data
 
 app = FastAPI(
     title="API Categorias",
     version="1.0.0",
     description="Endpoint para verificação das categorias dos livros."
 )
+
+
+dados_csv = get_csv_data(rf'C:\Users\Victor\Desktop\CONEXAO_REPO_fiap_tech_challenge_fase1\fiap_tech_challenge_fase1\data\books.csv')
+
+print(dados_csv)
 
 app.include_router(endpoint_categoria.router, prefix="")
 app.include_router(endpoint_categoria.router, prefix="/api/v1/categories")
