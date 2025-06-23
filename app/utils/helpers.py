d6f214ae0bd3b3a7f8ffe31215896f6c3b278524
Vitor Santiago <vitorsantiago14101997@yahoo.com>
2025-06-23 20:02:03 -0300

    Subindo versão melhorada da função que faz tratamento no dataframe lido do arquivo book.csv

--- app/utils/helpers.py
+++ app/utils/helpers.py
@@ -2,7 +2,14 @@ import pandas as pd
 
 def get_csv_data(path_file):
     df = pd.read_csv(path_file, sep=';')
-    df.fillna("",inplace=True)
+    #A coluna de data não vinha com o formato de data, vinha como objetc
+    df['created_at'] = pd.to_datetime(df['created_at'], errors='coerce')
+    #Evitando que tenham valores negativos nas colunas que tao como float
+    df['price_including_tax'] = df['price_including_tax'].apply(lambda x: abs(x))
+    df['price_excluding_tax'] = df['price_excluding_tax'].apply(lambda x: abs(x))
+    df['tax'] = df['tax'].apply(lambda x: abs(x))
+    #Verificando, os campos que vinham com valores vazios era o de categoria
+    df['category'] = df['category'].fillna("--")
     return df
 
 def get_only_categories(df):
