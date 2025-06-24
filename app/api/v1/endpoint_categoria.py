c1eb58e4bbddff1285edc80bb41313940ef41259
Vitor Santiago <vitorsantiago14101997@yahoo.com>
2025-06-23 21:19:00 -0300

    Subindo att.

--- app/api/v1/endpoint_categoria.py
+++ app/api/v1/endpoint_categoria.py
@@ -3,13 +3,15 @@ from app.utils.helpers import get_csv_data, get_only_categories
 
 router = APIRouter()
 
-dados_csv = get_csv_data(rf'data\books.csv')
-
+#dados_csv = get_csv_data(rf'data\books.csv')
+#dados_csv = get_csv_data(rf'data\books_empty.csv')
+dados_csv = get_csv_data(rf'data\books_without_data.csv')
 
 @router.get("/")
 async def home():
     return  "Somente para evitar erros"
 
+#Padronizar o retorno para aquilo que o Iago falou
 @router.get("/api/v1/categories")
 async def get_categories():
     dados_filtrados = get_only_categories(dados_csv)
