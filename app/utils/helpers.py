4989c4d92bc7def776baf7073d0ce9eef2d01eaa
Vitor Santiago <vitorsantiago14101997@yahoo.com>
2025-06-23 21:17:49 -0300

    Adicionando Try/Except na função que define os dados filtrados somente com categorias.

--- app/utils/helpers.py
+++ app/utils/helpers.py
@@ -1,17 +1,24 @@
 import pandas as pd
 
 def get_csv_data(path_file):
-    df = pd.read_csv(path_file, sep=';')
-    #A coluna de data não vinha com o formato de data, vinha como objetc
-    df['created_at'] = pd.to_datetime(df['created_at'], errors='coerce')
-    #Evitando que tenham valores negativos nas colunas que tao como float
-    df['price_including_tax'] = df['price_including_tax'].apply(lambda x: abs(x))
-    df['price_excluding_tax'] = df['price_excluding_tax'].apply(lambda x: abs(x))
-    df['tax'] = df['tax'].apply(lambda x: abs(x))
-    #Verificando, os campos que vinham com valores vazios era o de categoria
-    df['category'] = df['category'].fillna("--")
+    try:
+        df = pd.read_csv(path_file, sep=';')
+        #A coluna de data não vinha com o formato de data, vinha como objetc
+        df['created_at'] = pd.to_datetime(df['created_at'], errors='coerce')
+        #Evitando que tenham valores negativos nas colunas que tao como float
+        df['price_including_tax'] = df['price_including_tax'].apply(lambda x: abs(x))
+        df['price_excluding_tax'] = df['price_excluding_tax'].apply(lambda x: abs(x))
+        df['tax'] = df['tax'].apply(lambda x: abs(x))
+        #Verificando, os campos que vinham com valores vazios era o de categoria
+        df['category'] = df['category'].fillna("--")
+    except Exception as error:
+        return error
     return df
 
+#Aplicar um try para caso não seja possível fazer o filtro
 def get_only_categories(df):
-    df = df["category"].drop_duplicates()
+    try:
+        df = df["category"].drop_duplicates()
+    except Exception as error:
+        return  error
     return df
