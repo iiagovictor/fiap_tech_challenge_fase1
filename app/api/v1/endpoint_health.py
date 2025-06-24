dfc8296c8cf6e8ca5491e4aa41bde47cfbce6273
Vitor Santiago <vitorsantiago14101997@yahoo.com>
2025-06-23 21:18:39 -0300

    Fazendo uso do método HTTPException para fazer tratamento dos erros.

--- app/api/v1/endpoint_health.py
+++ app/api/v1/endpoint_health.py
@@ -1,16 +1,23 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from app.api.v1.endpoint_categoria import dados_csv
 
 router = APIRouter()
 
 @router.get("/api/v1/health")
 async def health():
-    if dados_csv.empty:
-        return{
-            "Status da API": "Erro",
-            "Msg": "Arquivo CSV não possui registros"
+    try:
+        if dados_csv.empty:
+
+            raise HTTPException(
+                status_code=400,
+                detail="Arquivo CSV não está populado."
+            )
+        return {
+            "Status da API": 200,
+            "Quantidade de registros no CSV": len(dados_csv)
         }
-    return {
-        "Status da API": 200,
-        "Quantidade de registros no CSV": len(dados_csv)
-    }
+    except Exception as error:
+        raise   HTTPException(
+            status_code=500,
+            detail=f"Erro interno {error}"
+        )
