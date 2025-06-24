bf048db8d4c7355bedf7097a9993d64106959a9d
Vitor Santiago <vitorsantiago14101997@yahoo.com>
2025-06-24 20:17:34 -0300

    Inserindo o raise do erro de requisição no endpoint de retorno das categorias dos livros.

--- app/api/v1/endpoint_categoria.py
+++ app/api/v1/endpoint_categoria.py
@@ -1,18 +1,38 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from app.utils.helpers import get_csv_data, get_only_categories
 
 router = APIRouter()
 
-#dados_csv = get_csv_data(rf'data\books.csv')
+dados_csv = get_csv_data(rf'data\books.csv')
 #dados_csv = get_csv_data(rf'data\books_empty.csv')
-dados_csv = get_csv_data(rf'data\books_without_data.csv')
+#dados_csv = get_csv_data(rf'data\books_without_data.csv')
 
 @router.get("/")
 async def home():
     return  "Somente para evitar erros"
 
-#Padronizar o retorno para aquilo que o Iago falou
+
 @router.get("/api/v1/categories")
 async def get_categories():
-    dados_filtrados = get_only_categories(dados_csv)
-    return  dados_filtrados
+    try:
+        dados_filtrados = get_only_categories(dados_csv)
+        if len(dados_filtrados) == 0:
+            raise HTTPException(
+                status_code=400,
+                detail="Arquivo CSV não está populado."
+            )
+        return {
+            "Success": True,
+            "Message": "Categorias retornadas com sucesso.",
+            "Data":{
+                "Categories":dados_filtrados
+            }
+        }
+    except HTTPException as http_error:
+        raise http_error 
+
+    except Exception as error:
+        raise   HTTPException(
+            status_code=500,
+            detail=f"Erro interno : {error}"
+        )
