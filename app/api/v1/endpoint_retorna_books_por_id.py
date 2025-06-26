cd8e6b244ccbc5d57217d49b898cc6d6c2a2f11d
Vitor Santiago <vitorsantiago14101997@yahoo.com>
2025-06-26 18:03:32 -0300

    🧪 Retirando prints.

--- app/api/v1/endpoint_retorna_books_por_id.py
+++ app/api/v1/endpoint_retorna_books_por_id.py
@@ -1,17 +1,12 @@
 from fastapi import APIRouter, HTTPException
 from app.api.v1.endpoint_categoria import dados_csv
-from pydantic import BaseModel
 
 router = APIRouter()
-
-print(len(dados_csv))
-print(type(dados_csv))
 
 @router.get("/api/v1/books/{book_id}")
 async def get_book_by_id(book_id: int):
     if 0 < book_id <= len(dados_csv):
         resultado = dados_csv[dados_csv["book_id"] == book_id]
-        print(resultado)
         return {
             "Success": True,
             "Message": "Categorias retornadas com sucesso.",
