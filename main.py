952fcc47a1c8b392621c6343c5ed395a1010f2a5
Vitor Santiago <vitorsantiago14101997@yahoo.com>
2025-06-26 18:09:08 -0300

    🩺 Subindo arquivo main.py na raiz correta e com as rotas.

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from app.api.v1 import endpoint_categoria, endpoint_health, endpoint_retorna_books_por_id
+
 
 app = FastAPI(
     title="Our FastAPI API",
@@ -6,6 +8,6 @@ app = FastAPI(
     description="API de Consultas de Livros com FASTAPI"
 )
 
-@app.get("/")
-async def home():
-    return "Página Inicial!"
+app.include_router(endpoint_categoria.router)
+app.include_router(endpoint_retorna_books_por_id.router)
+app.include_router(endpoint_health.router)
