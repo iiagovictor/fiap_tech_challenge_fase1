cb8c6f9ce24d59491b59fa078284ecc50140ed41
Vitor Santiago <vitorsantiago14101997@yahoo.com>
2025-06-27 19:31:55 -0300

    ✈️ Introduzindo a rota de overview no arquivo main.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from app.api.v1 import endpoint_categoria, endpoint_health, endpoint_retorna_books_por_id
+from app.api.v1 import endpoint_categoria, endpoint_health, endpoint_retorna_books_por_id, endpoint_overview
 
 
 app = FastAPI(
@@ -11,3 +11,4 @@ app = FastAPI(
 app.include_router(endpoint_categoria.router)
 app.include_router(endpoint_retorna_books_por_id.router)
 app.include_router(endpoint_health.router)
+app.include_router(endpoint_overview.router)
