5c157c4aa2295df38caea396604bf8ad723de5bd
JDSdamasceno <jadersondamascenodelima@gmail.com>
2025-06-28 14:15:21 -0300

    incluido a rota do Endpoint lista livros disponiveis

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from app.api.v1 import endpoint_categoria, endpoint_health, endpoint_retorna_books_por_id, endpoint_overview
+from app.api.v1 import endpoint_categoria, endpoint_health, endpoint_retorna_books_por_id, endpoint_overview, endpoint_lista_livros_disponiveis
 
 
 app = FastAPI(
@@ -12,3 +12,4 @@ app.include_router(endpoint_categoria.router)
 app.include_router(endpoint_retorna_books_por_id.router)
 app.include_router(endpoint_health.router)
 app.include_router(endpoint_overview.router)
+app.include_router(endpoint_lista_livros_disponiveis.router)
