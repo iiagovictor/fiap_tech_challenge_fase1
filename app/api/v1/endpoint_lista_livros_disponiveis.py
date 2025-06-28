9bbc2ee86ee5d09d297700991bcfc315bf5981ff
JDSdamasceno <jadersondamascenodelima@gmail.com>
2025-06-28 14:14:49 -0300

    Incluido a função get_unique_items

--- app/api/v1/endpoint_lista_livros_disponiveis.py
+++ app/api/v1/endpoint_lista_livros_disponiveis.py
@@ -1,15 +1,14 @@
 from fastapi import APIRouter, HTTPException
-from app.utils.helpers import get_csv_data, get_only_books
+from app.utils.helpers import get_csv_data, get_only_books, get_unique_items
 
 router = APIRouter()
 
 dados_csv = get_csv_data(rf'data\books.csv')
 
 @router.get("/api/v1/books:")
-
 async def get_books():
     try:
-        livros_filtrados = get_only_books(dados_csv)
+        livros_filtrados = get_unique_items(dados_csv,"title")
         if len(livros_filtrados) == 0:
             raise HTTPException(
                 status_code=400,
