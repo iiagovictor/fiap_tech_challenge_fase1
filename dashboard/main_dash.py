24dc566a703c4a9901403036294eaa5684cdcd8d
Luis Rodrigues <luis_henrique993@live.com>
2025-08-11 18:45:16 -0300

    [FIX] flake8 v1.2

--- dashboard/main_dash.py
+++ dashboard/main_dash.py
@@ -3,13 +3,13 @@ import sys
 import streamlit as st
 import pandas as pd
 
-
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from app.models.databases.base import SessionLocal
 from app.models.databases.logs import Log
 
+
 class RequestDashboard:
-    
+
     def __init__(self):
         self.df = self.load_data()
 
@@ -28,7 +28,7 @@ class RequestDashboard:
                     "method": log.method,
                     "latency": log.latency
                 }
-            for log in logs
+                for log in logs
             ]
             df = pd.DataFrame(data)
             return df
@@ -41,31 +41,31 @@ class RequestDashboard:
             return (
                 self.df['endpoint']
                 .value_counts()
-                .reset_index() \
-                .rename(columns={'index': 'endpoint', })
+                .reset_index(name='count')
+                .rename(columns={'index': 'endpoint'})
             )
         return pd.DataFrame()
-    
+
     def status_code_distribution(self):
         if not self.df.empty:
             return (
                 self.df['status_code']
                 .value_counts()
-                .reset_index() \
+                .reset_index(name='count')
                 .rename(columns={'index': 'status_code'})
             )
         return pd.DataFrame()
-    
+
     def average_latency_per_endpoint(self):
         if not self.df.empty:
             return (
                 self.df.groupby('endpoint')['latency']
                 .mean()
-                .reset_index() \
+                .reset_index()
                 .rename(columns={'latency': 'average_latency'})
             )
         return pd.DataFrame()
-    
+
     def total_requests_over_time(self):
         if not self.df.empty:
             self.df['time'] = pd.to_datetime(self.df['time'])
@@ -76,34 +76,36 @@ class RequestDashboard:
                 .reset_index(name='count')
             )
         return pd.DataFrame()
-    
+
+
 def main():
     st.title("Dashboard de Requisições")
-    
+
     dash = RequestDashboard()
-    
+
     col2, col3 = st.columns(2)
     col4, col5 = st.columns(2)
-    
+
     with col2:
         teste = dash.num_requests_per_endpoint()
         st.subheader("Número de requisições por endpoint")
         st.bar_chart(teste, x='endpoint', y='count')
-    
+
     with col3:
         st.subheader("Distribuição dos códigos de status")
-        status_dist = dash.status_code_distribution()   
+        status_dist = dash.status_code_distribution()
         st.bar_chart(status_dist, x='status_code', y='count')
-        
+
     with col4:
         st.subheader("Latência média por endpoint")
-        avg_latency = dash.average_latency_per_endpoint()   
+        avg_latency = dash.average_latency_per_endpoint()
         st.bar_chart(avg_latency, x='endpoint', y='average_latency')
-    
+
     with col5:
         st.subheader("Total de requisições ao longo do tempo")
         total_requests = dash.total_requests_over_time()
         st.line_chart(total_requests, x='time', y='count')
-    
+
+
 if __name__ == "__main__":
     main()
